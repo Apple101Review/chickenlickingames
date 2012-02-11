610d14236f932c3b2651b1f4b5e406eb71248e62
Shawn Schwartz <waverkid97@gmail.com>
2012-02-10 22:35:15 -0800

    Adds gravity python project to the repo.

---
+++ gravity.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+print ("Welcome to the Force of Gravity Program")
+massOne = int(raw_input()) + ("Please input the first mass here: ")
+massTwo = int(raw_input()) + ("Please input the second mass here: ")
+print ("So the first mass is ") +massOne ("and the second mass is: ") +massTwo
+
